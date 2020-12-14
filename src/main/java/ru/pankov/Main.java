1df146e0de953a5ad15516f4d6040d358087f3b3
MaximPnk <adm.mjkee@yandex.ru>
2020-12-14 20:57:07 +0300

    work finished

---
+++ src/main/java/ru/pankov/Main.java
@@ -0,0 +1,27 @@
+package ru.pankov;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.net.URL;
+import java.security.ProtectionDomain;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server(8189);
+
+        ProtectionDomain domain = Main.class.getProtectionDomain();
+        URL location = domain.getCodeSource().getLocation();
+
+        WebAppContext webAppContext = new WebAppContext();
+        webAppContext.setContextPath("/");
+        webAppContext.setWar(location.toExternalForm());
+        webAppContext.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed", "false");
+
+        server.setHandler(webAppContext);
+        server.start();
+        server.join();
+    }
+}
